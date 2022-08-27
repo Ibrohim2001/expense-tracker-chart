3afaabcc88f5bed99abad3512016c2673922fb61
Ibrohim2001 <ikhayitmirzaev@gmail.com>
2022-08-28 00:49:03 +0500

    first-commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const chartContainer = document.querySelector('.chart_container');
+const bgColor = 'hsl(186, 34%, 60%)';
+const currentClass = 'chart_indicator current';
+const chartIndicator = 'chart_indicator'
+fetch('./data.json').then(res => res.json()).then(data => getData(data));
+function getData(data) {
+  const currentDay = new Date().getDay();
+  data.map(item => {
+    const currentIndex = data.indexOf(item) + 1;
+    const height = 250 / 100 * Math.floor(item.amount);
+    chartContainer.innerHTML += `
+      <div class="chart_item">
+        <div class="${currentDay !== currentIndex ? chartIndicator : currentClass}" style="height:${height}px;">            
+          <p class="daily_spending">$<span class="daily_amount">${item.amount}</span></p>
+        </div>
+        <p class="weekday">${item.day}</p>
+      </div>
+    `;
+  });
+}
+{/* <div class="chart_indicator" style="background-color: ${currentDay === currentIndex && bgColor}; height:${height}px;"> */}
