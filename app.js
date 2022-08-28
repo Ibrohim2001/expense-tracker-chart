0a659c23dfeb57f2c8476aafbf18d6e5ce176dc3
Ibrohim2001 <ikhayitmirzaev@gmail.com>
2022-08-28 12:28:52 +0500

    js

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ fetch('./data.json').then(res => res.json()).then(data => getData(data));
 
 function getData(data) {
   const currentDay = new Date().getDay();
-  // console.log(currentDay) 
-  console.log(new Date().getUTCDay())
   data.map(item => {
     const currentIndex = data.indexOf(item);
     console.log(currentIndex - 1)
     const height = 250 / 100 * Math.floor(item.amount);
     chartContainer.innerHTML += `
-      <div class="chart_item">  
-        <div class="chart_indicator" style="background-color: ${currentDay === currentIndex && bgColor}; height:${height}px;">         
+      <div class="chart_item">
+      <div class="chart_indicator" style="background-color: ${currentDay === currentIndex && bgColor}; height:${height}px;">            
           <p class="daily_spending">$<span class="daily_amount">${item.amount}</span></p>
         </div>
         <p class="weekday">${item.day}</p>
@@ -23,5 +21,9 @@ function getData(data) {
     `;
   });
 }
-{/* <div class="${currentDay === currentIndex ? currentClass : chartIndicator}" style="height:${height}px;">  */}
+
+// Here's the div tag that I want to give cyan background, only need to change the div inside of the chart_item div
+// < class="${currentDay !== currentIndex ? chartIndicator : currentClass}" style="height:${height}px;">
+//  <p class="daily_spending">$<span class="daily_amount">${item.amount}</span></p>
+// </div>
 
